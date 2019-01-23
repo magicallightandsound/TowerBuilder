87da2113ebd735a25e12c1416007134178b65767
Rodney Degracia <rdegraci@gmail.com>
2019-01-22 21:58:30 -0500

    Initial working ActsAsRotatingMarker script and tests

---
+++ Assets/Scripts/ActsAsRotatingMarker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActsAsRotatingMarker : MonoBehaviour {
+
+    [SerializeField]
+    public float timeout = .15f;
+    public float degrees = 6f;
+
+    private float timeSinceLastRequest = 0f;
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeSinceLastRequest += Time.deltaTime;
+        if (timeSinceLastRequest > timeout)
+        {
+            timeSinceLastRequest = 0f;
+
+            // Rotate around the Y axis, 
+            transform.Rotate(new Vector3(0, 1, 0), degrees);
+        }
+    }
+}
---
+++ Assets/Tests/ActsAsRotatingMarkerTestScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+
+public class ActsAsRotatingMarkerTestScript {
+
+    [Test]
+    public void ShouldHaveDefaultTimeoutOf15F()
+    {
+        var gameObject = new GameObject("ActsAsRotatingMarkerGameObject");
+        gameObject.AddComponent<ActsAsRotatingMarker>();
+
+        ActsAsRotatingMarker actsAsRotatingMarker = gameObject.GetComponent<ActsAsRotatingMarker>();
+
+        Assert.IsInstanceOf<ActsAsRotatingMarker>(actsAsRotatingMarker);
+
+        Assert.IsTrue(actsAsRotatingMarker.timeout == .15f);
+    }
+
+    [Test]
+    public void ShouldHaveDefaultDegreesOf6F()
+    {
+        var gameObject = new GameObject("ActsAsRotatingMarkerGameObject");
+        gameObject.AddComponent<ActsAsRotatingMarker>();
+
+        ActsAsRotatingMarker actsAsRotatingMarker = gameObject.GetComponent<ActsAsRotatingMarker>();
+
+        Assert.IsInstanceOf<ActsAsRotatingMarker>(actsAsRotatingMarker);
+
+        Assert.IsTrue(actsAsRotatingMarker.degrees == 6f);
+    }
+}
