206bd88242d63e7024d0e29a3509e9d834b286dd
Rodney Degracia <rdegraci@gmail.com>
2019-01-24 00:07:23 -0500

    Initial ActsAsEyeTracker and ActsAsEyeTarget scripts

---
+++ Assets/Scripts/ActsAsEyeTarget.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActsAsEyeTarget : MonoBehaviour {
+
+    public bool coolDown = false;
+
+    private float timeSinceLastRequest = 0F;
+    private float timeout = 3F;
+
+    [SerializeField]
+    private float cooldownDelta = -0.1F;
+
+    [SerializeField]
+    public float heatupDelta = 0.1F;
+
+
+    private Vector3 initialScale;
+    private Vector3 maxScale;
+
+    // Use this for initialization
+    void Start () {
+        initialScale = gameObject.transform.localScale;
+        maxScale = new Vector3(initialScale.x, initialScale.y, initialScale.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (coolDown)
+        {
+            timeSinceLastRequest -= Time.deltaTime;
+
+            // Animated Cooldown 
+            Vector3 scale = this.gameObject.transform.localScale;
+            scale.y += cooldownDelta;
+            this.gameObject.transform.localScale = scale;
+        }
+        else
+        {
+            timeSinceLastRequest += Time.deltaTime;
+
+            // Animated Heatup
+            Vector3 scale = this.gameObject.transform.localScale;
+            scale.y += heatupDelta;
+            this.gameObject.transform.localScale = scale;
+        }
+
+        if (timeSinceLastRequest < 0)
+        {
+            // Animate full cold
+            this.gameObject.transform.localScale = initialScale;
+
+            timeSinceLastRequest = 0f;
+            return;
+        }
+
+
+        if (timeSinceLastRequest > timeout)
+        {
+            // Animated full heat
+            this.gameObject.transform.localScale = maxScale;
+            timeSinceLastRequest = 0f;
+        }
+    }
+
+    // Called by the ActsAsEyeTracker, when this ActsAsTarget is
+    // being targetted by the ActsAsEyeTracker raycast
+    public void OnTarget()
+    {
+        coolDown = false;
+    }
+
+    // Called by the ActsAsEyeTracker, when this ActsAsTarget is
+    // being no longer targetted by the ActsAsEyeTracker raycast
+    public void OffTarget()
+    {
+        coolDown = true;
+    }
+}
---
+++ Assets/Tests/ActsAsEyeTargetTestScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class ActsAsEyeTargetTestScript {
+
+    /*
+     * When the player looks at the game object that has the 
+     * ActsAsEyeTarget script, we count how many seconds the 
+     * Player has looked at the target. 
+     *
+     * When the cooldown is true, the count goes down (cools)
+     * but when the cooldown is false, the count goes up (heats)
+     */
+    [Test]
+    public void ShouldHaveEyeGazeCoolDownFlag ()
+    {
+        var gameObject = new GameObject("ActsAsEyeTargetGameObject");
+        gameObject.AddComponent<ActsAsEyeTarget>();
+
+        ActsAsEyeTarget actsAsEyeTarget = gameObject.GetComponent<ActsAsEyeTarget>();
+
+        Assert.IsInstanceOf<ActsAsEyeTarget>(actsAsEyeTarget);
+
+        Assert.IsFalse(actsAsEyeTarget.coolDown == true);
+
+    }
+
+    [Test]
+    public void NewTestScriptSimplePasses() {
+        // Use the Assert class to test conditions.
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses() {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        yield return null;
+    }
+}
